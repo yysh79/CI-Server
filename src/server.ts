93e516f823c2549f171d8e690ca331568762c9cf
Shalom Gispan <shalomgispan1@gmail.com>
2024-10-01 13:32:01 +0300

    fix excel issues

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { connectDatabase } from './config/mongoDbConect.js';
 import indexRout from './routes/indexRout.js'
 import express from 'express'
+const cors = require('cors'); // Import CORS middleware
 
 connectDatabase();
 
 const app = express();
+
+
+app.use(cors()); // Use the CORS middleware
 
 app.use(express.json())
 app.use('/', indexRout);
