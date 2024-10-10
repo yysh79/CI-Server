6858205371bf05dc772f4d76d807382f5c87a2d5
Shalom Gispan <shalomgispan1@gmail.com>
2024-10-10 15:27:24 +0300

    fixed

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -263,6 +263,7 @@ export const login = async (req: Request, res: Response) => {
 
     try {
         const user = await User.findOne({ email }); 
+        console.log("Retrieved User Object:", user); 
 
         if (!user) { 
              res.status(404).json(createServerResponse(false, null, 'משתמש לא נמצא !'));
