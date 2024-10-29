9f82ae5eb6e5394b63d0558c8a52f9bae0ba52dd
orel123 <oral.yosf.h@gmail.com>
2024-10-29 14:39:26 +0200

    הסרת תיקיית build ממעקב ב-Git

--- build/routes/fromRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const formRoutes = (0, express_1.Router)();
-formRoutes.get('/getAllForms', userController_1.getAllForms);
-exports.default = formRoutes;
